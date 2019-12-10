378fcb1ce261207e3a67facbc1fa9d1f2c636bcb
Anh Dương Việt <anh.dv@teko.vn>
2019-12-10 16:58:16 +0700

    Update create session for unix request

--- manager/services/docker/__init__.py
+++ manager/services/docker/__init__.py
@@ -18,28 +18,29 @@ class DockerManager(object):
 
     def __init__(self):
         self.__BASE_URL = "http+unix://%2Fvar%2Frun%2Fdocker.sock"
-        self.__session = requests_unixsocket.Session()
 
     def get_containers(self, container_id: str = None):
-        response = self.__session.get(f"{self.__BASE_URL}/containers/json?all=1")
-        ret = []
-        if response.status_code == const.HTTP_200:
-            res = response.json()
-            ret = [self.__received_containers(container) for container in res]
+        with requests_unixsocket.Session() as session:
+            response = session.get(f"{self.__BASE_URL}/containers/json?all=1")
+            ret = []
+            if response.status_code == const.HTTP_200:
+                res = response.json()
+                ret = [self.__received_containers(container) for container in res]
 
-        if container_id is not None:
-            ret = [x for x in ret if container_id in x.get("id")]
+            if container_id is not None:
+                ret = [x for x in ret if container_id in x.get("id")]
 
-        return ret
+            return ret
 
     def container_inspect(self, container_id: str):
-        resp = self.__session.get(f'{self.__BASE_URL}/containers/{container_id}/json')
-        if resp.status_code != const.HTTP_200:
-            raise Exception(f'Inspect container {container_id} failed with code: {resp.status_code}')
+        with requests_unixsocket.Session() as session:
+            resp = session.get(f'{self.__BASE_URL}/containers/{container_id}/json')
+            if resp.status_code != const.HTTP_200:
+                raise Exception(f'Inspect container {container_id} failed with code: {resp.status_code}')
 
-        ret = convert_camel_to_snake(resp.json())
+            ret = convert_camel_to_snake(resp.json())
 
-        return ret
+            return ret
 
     def container_stop(self, container_id: str):
         return self.__action_with_container("stop", container_id)
@@ -55,24 +56,25 @@ class DockerManager(object):
         This method return a stream
         """
         self.__check_container_existed(container_id)
-
-        response = self.__session.get(f"{self.__BASE_URL}/containers/{container_id}/logs?stderr=1&stdout=1&tail=all&follow=1",
-                                      stream=True)
-        is_tty_ = self.is_tty(container_id)
-        if is_tty_:
-            for line in response.iter_lines():
-                yield line + b"\n"
-        else:
-            for line in response.iter_lines():
-                yield line[8:] + b"\n"
+        with requests_unixsocket.Session() as session:
+            with session.get(f"{self.__BASE_URL}/containers/{container_id}/logs?stderr=1&stdout=1&tail=all&follow=1",
+                             stream=True) as response:
+                is_tty_ = self.is_tty(container_id)
+                if is_tty_:
+                    for line in response.iter_lines():
+                        yield line + b"\n"
+                else:
+                    for line in response.iter_lines():
+                        yield line[8:] + b"\n"
 
     def events(self):
         """
         This method return a stream
         """
-        response = self.__session.get(f"{self.__BASE_URL}/events", stream=True)
-        for line in response.iter_lines():
-            yield line + b"\n"
+        with requests_unixsocket.Session() as session:
+            with session.get(f"{self.__BASE_URL}/events", stream=True) as response:
+                for line in response.iter_lines():
+                    yield line + b"\n"
 
     def is_tty(self, container_id):
         container = self.container_inspect(container_id)
@@ -81,15 +83,16 @@ class DockerManager(object):
         return tty if tty is not None else False
 
     def __action_with_container(self, action, container_id):
-        self.__check_container_existed(container_id)
-        response = self.__session.post(f"{self.__BASE_URL}/containers/{container_id}/{action}?t=1")
-        if response.status_code != const.HTTP_204:
-            raise Exception(f"{action} container {container_id} failed: HTTP_CODE: {response.status_code}")
+        with requests_unixsocket.Session() as session:
+            self.__check_container_existed(container_id)
+            response = session.post(f"{self.__BASE_URL}/containers/{container_id}/{action}?t=1")
+            if response.status_code != const.HTTP_204:
+                raise Exception(f"{action} container {container_id} failed: HTTP_CODE: {response.status_code}")
 
-        return {
-            "code": 200,
-            "message": "Success"
-        }
+            return {
+                "code": 200,
+                "message": "Success"
+            }
 
     def __check_container_existed(self, container_id):
         container = self.get_containers(container_id)
